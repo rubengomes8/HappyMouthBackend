e9981c749112e1adba2e3a30eb5ea28ff99ef0f2
rubengomes8 <rsantosgomes@ext.worten.pt>
2023-10-18 21:46:17 +0100

    golang script instead of python

---
+++ cmd/scripts/populate_cache/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+
+	// 1. Abrir CSV e guardar filepaths dos JSON para processar
+	// 2. Por cada ficheiro JSON, ler a receita e guardar numa BD
+
+}
--- cmd/scripts/populate_cache/main.py
+++
@@ -1,4 +0,0 @@
-"""
-1. Abrir CSV e guardar filepaths dos JSON para processar
-2. Por cada ficheiro JSON, ler a receita e guardar numa BD
-"""
