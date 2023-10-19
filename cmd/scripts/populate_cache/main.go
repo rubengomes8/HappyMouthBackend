2ab6e405f0f72557174ff3e517faaca1946b1386
MgomesPt92 <micaelsgomes@gmail.com>
2023-10-19 19:10:26 +0100

    define first 3 steps

--- cmd/scripts/populate_cache/main.go
+++ cmd/scripts/populate_cache/main.go
@@ -2,7 +2,11 @@ package main
 
 func main() {
 
-	// 1. Abrir CSV e guardar filepaths dos JSON para processar
+	// 1. Abrir CSV
+	// 2. Construir Mapa(Dicionário) map[string]string
+	// 3. Fazer print do mapa
+
+	// 2. Guardar filepaths dos JSON para processar
 	// 2. Por cada ficheiro JSON, ler a receita e guardar numa BD
 
 }
