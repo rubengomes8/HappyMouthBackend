ea01f48c5fa67abc7c329f5580d8ff179d7593ca
rubengomes8 <rsantosgomes@ext.worten.pt>
2023-10-19 19:45:16 +0100

    divide main in 2 parts

--- cmd/scripts/populate_cache/main.go
+++ cmd/scripts/populate_cache/main.go
@@ -2,9 +2,18 @@ package main
 
 func main() {
 
+	/* First Part */
 	// 1. Abrir CSV
 	// 2. Construir Mapa(Dicionário) map[string]string
-	// 3. Fazer print do mapa
+	// 3. Fazer print do mapa fmt.Println(varname)
+	/* expected map
+	{
+		"meat,oregon,potato|onion": "recipes/recipe1.json",
+		"mushroom,tomato|honey,onion": "recipes/recipe2.json"
+	}
+	*/
+
+	/* Second Part */
 	// 4. Guardar filepaths dos JSON para processar
 	// 5. Por cada ficheiro JSON, ler a receita e guardar numa BD
 
