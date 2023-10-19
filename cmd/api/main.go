efc55a42deacd1c1416a925c7616d472a72cc50f
rubengomes8 <rsantosgomes@ext.worten.pt>
2023-10-19 21:28:54 +0100

    remove unused redis

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,26 +7,19 @@ import (
 
 	"github.com/rubengomes8/HappyMouthBackend/internal/ingredients"
 	"github.com/rubengomes8/HappyMouthBackend/pkg/kvstore"
-	"github.com/rubengomes8/HappyMouthBackend/pkg/redis"
 )
 
 func main() {
 
 	ctx := context.Background()
 
-	// REDIS
-	cache := redis.NewClient("localhost:6379", 0)
-	if err := cache.Ping(ctx); err != nil {
-		log.Fatal(err)
-	}
-
 	// DYNAMO DB
 	dynamoDBClient, err := kvstore.NewClient(ctx, "http://localhost:8000", "us-east-1")
-	if err := cache.Ping(ctx); err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	router, err := ingredients.NewAPI(cache, dynamoDBClient)
+	router, err := ingredients.NewAPI(dynamoDBClient)
 	if err != nil {
 		log.Fatal(err)
 	}
