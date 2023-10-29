cd66ecda49c3e6264f19d643e31d0adac20eefff
rubengomes8 <rsantosgomes@ext.worten.pt>
2023-10-29 20:21:44 +0000

    unify apis cmd

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,18 @@ import (
 	"context"
 	"log"
 
+	"github.com/IBM/sarama"
 	corepg "github.com/rubengomes8/HappyCore/pkg/postgres"
 	"github.com/rubengomes8/HappyMouthBackend/internal/auth"
 	"github.com/rubengomes8/HappyMouthBackend/internal/ingredients"
+	"github.com/rubengomes8/HappyMouthBackend/internal/recipegenerator"
 	"github.com/rubengomes8/HappyMouthBackend/pkg/kvstore"
+	"github.com/rubengomes8/HappyMouthBackend/pkg/redis"
+)
+
+const (
+	kafkaBrokerAddress = "localhost:9092"
+	useKafka           = false
 )
 
 func main() {
@@ -27,6 +35,25 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// REDIS
+	cache := redis.NewClient("localhost:6379", 0)
+	if err := cache.Ping(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	// KAFKA
+	var producer sarama.SyncProducer
+	if useKafka {
+		config := sarama.NewConfig()
+		config.Producer.Return.Successes = true
+
+		producer, err := sarama.NewSyncProducer([]string{kafkaBrokerAddress}, config)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer producer.Close()
+	}
+
 	// AUTH API - GIN + PGSQL GORM
 	err = auth.NewAPI(postgresDB).Run("localhost:8080")
 	if err != nil {
@@ -35,6 +62,12 @@ func main() {
 
 	// INGREDIENTS ROUTER - GIN + DYNAMO
 	err = ingredients.NewAPI(dynamoDBClient).Run("localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// RECIPE GENERATOR API - GIN + REDIS
+	err = recipegenerator.NewAPI(cache, producer).Run("localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
